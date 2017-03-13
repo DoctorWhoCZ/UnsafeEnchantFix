28d4d1c9fd5cf7cebd3f3b2f4b9fedd02599479c
Gwennaël Guichard <gwennaelguich@gmail.com>
2017-03-13 23:50:05 +0100

    Code improvements

--- src/main/java/me/rellynn/plugins/uenchantfix/PacketListener.java
+++ src/main/java/me/rellynn/plugins/uenchantfix/PacketListener.java
@@ -1,10 +1,8 @@
 package me.rellynn.plugins.uenchantfix;
 
-import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.reflect.StructureModifier;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.enchantments.Enchantment;
@@ -16,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import static com.comphenix.protocol.PacketType.Play.Server.SET_SLOT;
+import static com.comphenix.protocol.PacketType.Play.Server.WINDOW_ITEMS;
+
 /**
  * Created by gwennaelguich on 12/03/2017.
  */
@@ -23,12 +24,12 @@ public class PacketListener extends PacketAdapter {
     private UEnchantFix plugin;
 
     PacketListener(UEnchantFix plugin) {
-        super(plugin, PacketType.Play.Server.SET_SLOT, PacketType.Play.Server.WINDOW_ITEMS);
+        super(plugin, SET_SLOT, WINDOW_ITEMS);
         this.plugin = plugin;
     }
 
     private void fixItemStack(ItemStack item) {
-        if (item.hasItemMeta()) {
+        if (item != null && item.hasItemMeta()) {
             ItemMeta meta = item.getItemMeta();
             meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
             List<String> lore = new ArrayList<>();
@@ -44,19 +45,13 @@ public class PacketListener extends PacketAdapter {
 
     @Override
     public void onPacketSending(PacketEvent evt) {
-        PacketContainer packet = evt.getPacket().deepClone();
-        if (packet.getType() == PacketType.Play.Server.SET_SLOT) {
-            StructureModifier<ItemStack> modifier = packet.getItemModifier();
-            for (int i = 0; i < modifier.size(); i++)
-                fixItemStack(modifier.getValues().get(i));
-        } else {
-            StructureModifier<ItemStack[]> modifier = packet.getItemArrayModifier();
-            for (int i = 0; i < modifier.size(); i++) {
-                for (int j = 0; j < modifier.getValues().size(); j++) {
-                    if (modifier.getValues().get(i)[j] != null)
-                        fixItemStack(modifier.getValues().get(i)[j]);
-                }
-            }
+        PacketContainer packet = evt.getPacket();
+        if (packet.getType() == SET_SLOT) {
+            fixItemStack(packet.getItemModifier().read(0));
+        } else if (packet.getType() == WINDOW_ITEMS) {
+            ItemStack[] elements = packet.getItemArrayModifier().read(0);
+            for (ItemStack item : elements)
+                fixItemStack(item);
         }
         evt.setPacket(packet);
     }
